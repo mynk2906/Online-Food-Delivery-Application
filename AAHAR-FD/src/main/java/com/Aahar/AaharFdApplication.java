877246b95943503ee7c9e304b2d3cafc083e7fa8
mynk2906 <msaxena2906@gmail.com>
2023-06-01 13:16:41 +0530

    pom updated

---
+++ AAHAR-FD/src/main/java/com/Aahar/AaharFdApplication.java
@@ -0,0 +1,13 @@
+package com.Aahar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaharFdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaharFdApplication.class, args);
+	}
+
+}
